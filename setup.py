f4ba38ef6479182a9fe055d791b74af0bfc51a1d
Tobias Heinemann <tobias.heinemann@gmail.com>
2016-07-18 23:56:35 +0200

    Enable Cython's annotate feature by default

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from Cython.Compiler import Options
 from distutils.extension import Extension
 from numpy import get_include
 
 extensions = [Extension("*", ["*.pyx"], include_dirs=[get_include()])]
+Options.annotate = True
 setup(ext_modules=cythonize(extensions))
