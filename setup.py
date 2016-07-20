b174f67251e4c5322f9f331b2b0116039f9406e9
Tobias Heinemann <tobias.heinemann@gmail.com>
2016-07-20 23:20:51 +0200

    Don't change Cython directives on the command line

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from Cython.Compiler import Options
 from distutils.extension import Extension
 from numpy import get_include
 
+Options.annotate = False
+Options.directive_defaults["boundscheck"] = True
+Options.directive_defaults["cdivision"] = False
+
 extensions = [Extension("*", ["*.pyx"], include_dirs=[get_include()])]
-Options.annotate = True
 setup(ext_modules=cythonize(extensions))
