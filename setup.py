87f3736f6cf6ea8ab5fdcee6f84f8a7214e31976
Tobias Heinemann <tobias.heinemann@gmail.com>
2016-07-25 01:22:49 +0200

    Disable "-Wunused-function" and "-W#warnings"
    The code builds cleanly now.

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ from numpy import get_include
 Options.annotate = False
 Options.directive_defaults["boundscheck"] = True
 Options.directive_defaults["cdivision"] = False
+cflags = ["-Wno-unused-function", "-Wno-#warnings"]
 
-extensions = [Extension("*", ["*.pyx"], include_dirs=[get_include()])]
+extensions = [Extension(
+    "*", ["*.pyx"], include_dirs=[get_include()], extra_compile_args=cflags)]
 setup(ext_modules=cythonize(extensions))
