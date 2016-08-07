b8399b1afc0613bc9f98ef8bd45710c7da4c22ab
Tobias Heinemann <tobias.heinemann@gmail.com>
2016-08-07 13:45:07 +0200

    Cosmetics

--- tests/test_ionacoustic.py
+++ tests/test_ionacoustic.py
@@ -9,54 +9,49 @@ def test_ionacoustic(plot=False):
 
     # Quiet start
     quiet = True
-
     # Number of grid points in x- and y-direction
     nx, ny = 32, 32
-
-    # x- and y-grid
-    xg, yg = numpy.meshgrid(numpy.arange(0,nx), numpy.arange(0,ny))
-
     # Average number of particles per cell
     npc = 256
-
     # Particle charge and mass
     charge = 1.0
     mass = 1.0
-
     # Electron temperature
     Te = 1.0
+    # Dimensionless amplitude of perturbation
+    A = 0.001
+    # Wavenumbers
+    ikx = 1
+    iky = 1
+    # Thermal velocity of electrons in x- and y-direction
+    vtx, vty = 0.0, 0.0
+    # Timestep
+    dt = 0.1
+    # Number of periods to run for
+    nperiods = 1
+
+    # x- and y-grid
+    xg, yg = numpy.meshgrid(numpy.arange(nx), numpy.arange(ny))
 
     # Sound speed
     cs = numpy.sqrt(Te/mass)
 
-    # Thermal velocity of electrons in x- and y-direction
-    vtx, vty = 0.0, 0.0
-
     # Total number of particles in simulation
     np = npc*nx*ny
 
-    # Wavenumbers
-    ikx = 1
-    iky = 1
-    kx = ikx*2*numpy.pi/nx
-    ky = iky*2*numpy.pi/ny
-    k  = numpy.sqrt((kx**2+ky**2))
+    # Wave vector and its modulus
+    kx = 2*numpy.pi*ikx/nx
+    ky = 2*numpy.pi*iky/ny
+    k = numpy.sqrt(kx*kx + ky*ky)
 
     # Frequency
     omega = k*cs
 
     # Simulation time
-    nperiods = 1
-    tend = nperiods*2*numpy.pi/omega
-
-    # Timestep
-    dt = 0.1
+    tend = 2*numpy.pi*nperiods/omega
 
     # Number of time steps
     nt = int(tend/dt)
-
-    # Amplitude of perturbation
-    A = 0.001
 
     def rho_an(x, y, t):
         """Analytic density as function of x, y and t"""
@@ -72,7 +67,7 @@ def test_ionacoustic(plot=False):
 
     if quiet:
         # Uniform distribution of particle positions (quiet start)
-        assert(numpy.sqrt(npc) % 1 == 0)
+        assert numpy.sqrt(npc) % 1 == 0
         dx = 1/int(numpy.sqrt(npc))
         dy = dx
         X = numpy.arange(0, nx, dx)
@@ -85,8 +80,8 @@ def test_ionacoustic(plot=False):
         y = ny*numpy.random.uniform(size=np).astype(Float)
 
     # Perturbation to particle velocities
-    vx = ux_an(x, y, t = 0)
-    vy = uy_an(x, y, t = 0)
+    vx = ux_an(x, y, t=0)
+    vy = uy_an(x, y, t=0)
 
     # Add thermal velocity
     vx += vtx*numpy.random.normal(size=np).astype(Float)
@@ -114,11 +109,11 @@ def test_ionacoustic(plot=False):
 
     # Set the electric field to zero
     E = Field(grid, comm, dtype=Float2)
-    E.fill(0.0)
+    E.fill((0.0, 0.0))
 
     # Set force field to zero
     fxy = Field(grid, comm, dtype=Float2)
-    fxy.fill(0.0)
+    fxy.fill((0.0, 0.0))
 
     # Initialize sources
     sources = Sources(grid, comm, dtype=Float)
@@ -150,19 +145,18 @@ def test_ionacoustic(plot=False):
     if plot:
         import matplotlib.pyplot as plt
         global_rho = concatenate(sources.rho.trim())
-        global_E = concatenate(E.trim())
 
         if comm.rank == 0:
             plt.rc('image', origin='lower', interpolation='nearest')
             plt.figure(1)
-            fig, (ax1, ax2, ax3) = plt.subplots (num=1, ncols=3)
-            im1 = ax1.imshow(rho_an(xg, yg, 0),vmin=1-A,vmax=1+A)
-            im2 = ax2.imshow(rho_an(xg, yg, 0),vmin=1-A,vmax=1+A)
-            im3 = ax3.plot(xg[0,:], global_rho[0,:],'b',\
-                           xg[0,:], rho_an(xg, yg, 0)[0,:],'k--')
+            fig, (ax1, ax2, ax3) = plt.subplots(num=1, ncols=3)
+            im1 = ax1.imshow(rho_an(xg, yg, 0), vmin=1-A, vmax=1+A)
+            im2 = ax2.imshow(rho_an(xg, yg, 0), vmin=1-A, vmax=1+A)
+            im3 = ax3.plot(xg[0, :], global_rho[0, :], 'b',
+                           xg[0, :], rho_an(xg, yg, 0)[0, :], 'k--')
             ax1.set_title(r'$\rho$')
-            ax3.set_ylim(1-A,1+A)
-            ax3.set_xlim(0,x[-1])
+            ax3.set_ylim(1-A, 1+A)
+            ax3.set_xlim(0, x[-1])
 
     t = 0
     ##########################################################################
@@ -201,14 +195,14 @@ def test_ionacoustic(plot=False):
                 if comm.rank == 0:
                     im1.set_data(global_rho)
                     im2.set_data(rho_an(xg, yg, t))
-                    im3[0].set_ydata(global_rho[0,:])
-                    im3[1].set_ydata(rho_an(xg, yg, t)[0,:])
+                    im3[0].set_ydata(global_rho[0, :])
+                    im3[1].set_ydata(rho_an(xg, yg, t)[0, :])
                     plt.pause(1e-7)
 
     # Check if test has passed
     global_rho = concatenate(sources.rho.trim())
     if comm.rank == 0:
-        assert(numpy.max(numpy.abs(rho_an(xg, yg, t)-global_rho)) < 1e-4)
+        assert numpy.max(numpy.abs(rho_an(xg, yg, t) - global_rho)) < 1e-4
 
 if __name__ == "__main__":
     import argparse
