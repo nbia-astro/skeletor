358a4c69261734030d9b038855bed77d94c6d5f5
Thomas Berlok <berlok@nbi.dk>
2016-08-20 18:54:14 -0700

    Fixed initial guess for warm plasmas
    Runs so fast after I added a maxterms like
    you suggested!
    
    Added more comments and example of usage

--- dispersion_solvers/ionacoustic-disp.py
+++ dispersion_solvers/ionacoustic-disp.py
@@ -1,15 +1,29 @@
 class IonacousticDispersion:
+    """Class for solving the ionacoustic dispersion relation"""
 
-    def __init__ (self, Ti, Te, b=1, p=2, dx=1, tol=1e-4, maxterms=1000):
-        from numpy import sqrt
+    def __init__ (self, Ti, Te, b=1, p=2, dx=1, tol=1e-8,
+                  maxterms=1000, N=100):
+        from numpy import sqrt, log10, logspace
+        # Grid spacing
         self.dx = dx
+        # Ion temperature
         self.Ti = Ti
+        # Electron temperature
         self.Te = Te
+        # Alpha parameter
         self.alpha = sqrt(Ti/Te)
+        # Order of interpolation
         self.p = p
+        # Energy () or momentum conserving scheme
         self.b = b
+        # Tolerance for solutions
         self.tol = tol
+        # Max terms in aliasing sum
         self.maxterms = maxterms
+        # Number of points used to iterate in temperature
+        self.N = N
+        # Vector used to iterate alpha up to the target value
+        self.alpha_vec = logspace(-4, log10(self.alpha), self.N)
 
     def W(self, z):
         """Plasma response function"""
@@ -17,30 +31,30 @@ class IonacousticDispersion:
         from numpy import pi, sqrt
         return (1. + 1j*sqrt (0.5*pi)*z*wofz (sqrt (0.5)*z))
 
-    def det(self, vph, kx):
-        """Returns the expression that needs to be zero"""
+    def det(self, vph, kx, alpha):
+        """Returns the dispersion relation (that needs to be zero)"""
         from numpy import arange, sin, pi
-        # Number of terms included
         p = self.p
         b = self.b
         kdx = kx*self.dx
-        # n = 0 term
-        A = 1/kdx**(2*p-b+2)*self.W(vph/self.alpha)
+        # The n = 0 term
+        A = 1/kdx**(2*p-b+2)*self.W(vph/alpha)
 
         for j in range(1, self.maxterms):
             B = 0
             for n in (j, -j):
                 kn = kdx - 2*pi*n
-                B += 1/kn**(2*p-b+2)*self.W(vph/self.alpha/abs(1-2*pi*n/kdx))
+                B += 1/kn**(2*p-b+2)*self.W(vph/alpha/abs(1-2*pi*n/kdx))
             A += B
             if abs((B/A)) < self.tol:
                 A *=  sin(kdx)**(2-b)*(sin(kdx/2)/0.5)**(2*p)
-                return A + self.alpha**2
+                return A + alpha**2
         raise RuntimeError ("Exceeded maxterms={} aliasing terms!".\
             format (self.maxterms))
 
     def cold(self, kx):
-        """Cold limit of the dispersion relation"""
+        """Cold limit of the numerical dispersion relation. Returns the phase
+           velocity. Input: Single kx. Output: Real phase velocity"""
         from numpy import sin, cos, sqrt
         p = self.p
         b = self.b
@@ -63,28 +77,90 @@ class IonacousticDispersion:
                     /(180*dx**2))
         return omega/kx
 
-    def __call__ (self, kx):
-        """Solve warm dispersion relation using Newton's method. Returns omega
-        which has a real and imaginary part."""
+    def omega_vs_alpha(self, kx):
+        """Solve warm dispersion relation using Newton's method.
+           Input: Single kx value.
+           Output: The complex phase-velocity vph(alpha) where alpha
+           is a vector of length N with alpha[-1] = sqrt(Ti/Te). The value of
+           N can be set at object instantiation."""
         from scipy.optimize import newton
         from numpy import empty, complex128
+
+        det = self.det
+
+        alpha = self.alpha_vec
+
+        # Array for the complex phase-velocity
+        vph = empty (len (alpha), dtype = complex128)
+
+        # Initial guess from cold numerical dispersion relation
+        vph[0] = self.cold(kx)
+
+        for i in range(1, self.N):
+            vph[i] = newton (det, vph[i-1], args = (kx, alpha[i],))
+            assert(abs(det(vph[i], kx, alpha[i])) < self.tol), \
+            'Guess is not within the tolerance!'
+
+        return vph
+
+
+    def __call__ (self, kx):
+        """Solve warm dispersion relation using Newton's method.
+        Input: vector of kx values.
+        Output: The complex phase-velocity vph(kx). Note that omega = kx*vph.
+        The vector kx needs to be relatively finegrained. If this is not the
+        case, please use the method omega_vs_alpha instead.
+        """
+        from scipy.optimize import newton
+        from numpy import empty, complex128
+
+        dx    = self.dx
+        alpha = self.alpha
+        det   = self.det
 
         # Array for the complex phase-velocity
         vph = empty (len (kx), dtype = complex128)
 
-        # Initial guess from cold numerical dispersion relation
-        guess = self.cold(kx[0])
-        # First
-        vph[0] = newton (self.det, guess, args = (kx[0]*self.dx,))
+        # Guess from cold numerical and then iterated up to correct
+        # temperature ratio
+        vph[0] = self.omega_vs_alpha(kx[0])[-1]
+
         # Loop over kx
         for i in range (1, len (kx)):
-          vph[i] = newton (self.det, vph[i-1], args = (kx[i]*self.dx,))
-          # Check numerical solution is correct
-          assert(abs(self.det(vph[i], kx[i]*self.dx)) < self.tol), \
-          'Solution is not with the tolerance!'
+            vph[i] = newton (det, vph[i-1], args = (kx[i], alpha,))
+            # Check numerical solution is correct
+            assert(abs(det(vph[i], kx[i], alpha)) < self.tol), \
+            'Solution is not within the tolerance!'
 
-        # Frequency of the wave
-        omega = kx*vph
+        return vph
 
-        return omega
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    solve = IonacousticDispersion(Ti=1, Te=1)
+
+    # Fine-grained kx
+    kx = np.linspace(1e-4, 9*np.pi/10, 100)
+    vph = solve(kx)
+    plt.figure(1)
+    plt.plot(kx, vph.imag)
+
+    # Course-grained kx (as in simulations)
+    Nx = 64
+    Lx = Nx
+    vph = []
+    kxvec = np.arange(1, Nx//4)*2*np.pi/Lx
+    for kx in kxvec:
+        vph.append(solve.omega_vs_alpha(kx)[-1])
+    vph = np.array(vph)
+    plt.figure(2)
+    plt.plot(kxvec, vph.imag)
+
+    # Gamma vs Ti/Te for a fixed k
+    vph = solve.omega_vs_alpha(2*np.pi/Lx)
+    plt.figure(3)
+    plt.plot(solve.alpha_vec**2, vph.imag)
+
+    plt.show()
+
 
