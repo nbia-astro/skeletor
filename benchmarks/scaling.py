1d809766b104f844a4f33fe82d3d8ea07da27b0e
Thomas Berlok <tberlok@gmail.com>
2016-09-03 01:34:59 +0200

    Only do weak scaling and save scaling.txt

--- benchmarks/scaling.py
+++ benchmarks/scaling.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Number of processors on machine
-N = 32
+N = 1024
 
 def scaling(N, strong=False, mpifft=False):
     # Write header to file
@@ -33,35 +33,35 @@ def scaling(N, strong=False, mpifft=False):
     data = np.loadtxt('scaling.txt')
     n_procs = data[:,0]
     time    = data[:,1]
-    subprocess.call('rm scaling.txt', shell=True)
+    #subprocess.call('rm scaling.txt', shell=True)
 
     return (n_procs, time)
 
 
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # Weak scaling test
-plt.figure(1)
+#plt.figure(1)
 (n_procs, time) = scaling(N, strong=False, mpifft=False)
-plt.loglog(n_procs, time/n_procs, 'x-', label='PPIC2')
-(n_procs, time) = scaling(N, strong=False, mpifft=True)
-plt.loglog(n_procs, time/n_procs, 'x-', label='mpiFFT4py')
-plt.loglog(n_procs, time[0]/n_procs, 'k', label='Ideal')
-plt.legend(frameon=False)
-plt.xlabel('# processors')
-plt.ylabel('time/(# processors)')
-plt.savefig('weak_scaling.pdf')
+#plt.loglog(n_procs, time/n_procs, 'x-', label='PPIC2')
+#(n_procs, time) = scaling(N, strong=False, mpifft=True)
+#plt.loglog(n_procs, time/n_procs, 'x-', label='mpiFFT4py')
+#plt.loglog(n_procs, time[0]/n_procs, 'k', label='Ideal')
+#plt.legend(frameon=False)
+#plt.xlabel('# processors')
+#plt.ylabel('time/(# processors)')
+#plt.savefig('weak_scaling.pdf')
 
 
 # Strong scaling test
-plt.figure(2)
-(n_procs, time) = scaling(N, strong=True, mpifft=False)
-plt.loglog(n_procs, time, 'x-', label='PPIC2')
-(n_procs, time) = scaling(N, strong=True, mpifft=True)
-plt.loglog(n_procs, time, 'x-', label='mpiFFT4py')
-plt.loglog(n_procs, time[0]/n_procs, 'k', label='Ideal')
-plt.legend(frameon=False)
-plt.xlabel('# processors')
-plt.ylabel('time')
-plt.savefig('strong_scaling.pdf')
+#plt.figure(2)
+#(n_procs, time) = scaling(N, strong=True, mpifft=False)
+#plt.loglog(n_procs, time, 'x-', label='PPIC2')
+#(n_procs, time) = scaling(N, strong=True, mpifft=True)
+#plt.loglog(n_procs, time, 'x-', label='mpiFFT4py')
+#plt.loglog(n_procs, time[0]/n_procs, 'k', label='Ideal')
+#plt.legend(frameon=False)
+#plt.xlabel('# processors')
+#plt.ylabel('time')
+#plt.savefig('strong_scaling.pdf')
