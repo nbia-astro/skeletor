4bbd4df6b1b35947fa0dc635943886411ec69480
Thomas Berlok <berlok@nbi.dk>
2016-09-03 15:17:34 -0700

    Added detailed timing output to ppic2.py

--- benchmarks/ppic2.py
+++ benchmarks/ppic2.py
@@ -82,32 +82,52 @@ def ppic2(mpifft= False, indy = 9):
   # Main loop over time                                                    #
   ##########################################################################
   # Start the timer
+  t_push = 0
+  t_deposit = 0
+  t_add_guard = 0
+  t_copy_guard = 0
+  t_solve = 0
   tstart = MPI.Wtime()
   for it in range(nt):
 
       # Push particles on each processor. This call also sends and
       # receives particles to and from other processors/subdomains.
+      t_temp  = MPI.Wtime()
       electrons.push(E, dt)
+      t_push += MPI.Wtime() - t_temp
 
       # Update time
       t += dt
 
       # Deposit sources
+      t_temp  = MPI.Wtime()
       sources.deposit_ppic2(electrons)
+      t_deposit += MPI.Wtime() - t_temp
 
       # Boundary calls
+      t_temp  = MPI.Wtime()
       sources.rho.add_guards_ppic2()
+      t_add_guard += MPI.Wtime() - t_temp
 
       # Solve Gauss' law
+      t_temp  = MPI.Wtime()
       poisson(sources.rho, E, destroy_input=False)
+      t_solve += MPI.Wtime() - t_temp
 
       # Set boundary condition
+      t_temp  = MPI.Wtime()
       E.copy_guards_ppic2()
+      t_copy_guard += MPI.Wtime() - t_temp
 
   # Get elapsed time
   elapsed_time = MPI.Wtime() - tstart
   if comm.rank == 0:
-    print('{}\t{:.2f}\t{}\n'.format(comm.Get_size(), elapsed_time, ny))
+    print("Push time {}".format(t_push))
+    print("Deposit time {}".format(t_deposit))
+    print("Add guard time {}".format(t_add_guard))
+    print("Copy guard time {}".format(t_copy_guard))
+    print("Solve Gauss time {}".format(t_solve))
+    print("Total time {}".format(elapsed_time))
 
 if __name__ == "__main__":
     import argparse
