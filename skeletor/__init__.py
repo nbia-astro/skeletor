4c6f4f24fbd8616eaf6de689abc4e370088d552a
Thomas Berlok <berlok@nbi.dk>
2016-09-30 23:16:37 -0700

    Added velocity in initial_condition.py

--- skeletor/__init__.py
+++ skeletor/__init__.py
@@ -7,5 +7,6 @@ from .particle_sort import ParticleSort
 from .poisson import Poisson
 from .ohm import Ohm
 from .sources import Sources
-from .initial_condition import uniform_density
+from .math import log
+from .initial_condition import uniform_density, velocity_perturbation
 from .experiment import Experiment
