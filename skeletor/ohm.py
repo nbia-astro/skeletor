06a63179dbf889914faac084d183574ba2621d3e
Thomas Berlok <berlok@nbi.dk>
2016-10-18 12:10:06 -0700

    Simpl Ohm using Operators

--- skeletor/ohm.py
+++ skeletor/ohm.py
@@ -1,19 +1,11 @@
 class Ohm:
 
-    """Solve Ohm's law via a discrete Fourier transform."""
+    """Solve Ohm's law"""
 
     def __init__(self, grid, npc, charge=1.0, temperature=0.0, eta=0.0):
 
-        from numpy import zeros, zeros_like, array
-        from math import log2
-        from mpiFFT4py.line import R2C
-        from mpi4py import MPI
-
-        self.indx = int(log2(grid.nx))
-        self.indy = int(log2(grid.ny))
-
-        assert grid.nx == 2**self.indx, "'nx' needs to be a power of two"
-        assert grid.ny == 2**self.indy, "'ny' needs to be a power of two"
+        # Store the operators here for easy access
+        self.operators = grid.operators
 
         # Charge
         self.charge = charge
@@ -22,39 +14,14 @@ class Ohm:
         # Resistivity
         self.eta = eta
 
-        # Grid dimensions
-        N = array([grid.ny, grid.nx], dtype=int)
-        L = array([grid.Ly, grid.Lx], dtype=float)
-
-        # Create FFT object
-        self.FFT = R2C(N, L, MPI.COMM_WORLD, "double")
-
-        # Pre-allocate array for Fourier transform and force
-        self.lnrho_hat = zeros(
-                self.FFT.complex_shape(), dtype=self.FFT.complex)
-        self.Ex_hat = zeros_like(self.lnrho_hat)
-        self.Ey_hat = zeros_like(self.lnrho_hat)
-
-        # Define kx and ky (notice swapping due to the grid ordering)
-        self.ky, self.kx = self.FFT.get_scaled_local_wavenumbermesh()
-
+    @property
+    def alpha(self):
         # Ratio of temperature to charge
-        self.alpha = self.temperature/self.charge
+        return self.temperature/self.charge
 
     def __call__(self, rho, E, destroy_input=True):
-        from numpy import log
+        from skeletor import log
 
-        # Transform log of charge density to Fourier space
-        self.lnrho_hat[:] = self.FFT.fft2(log(rho.trim()), self.lnrho_hat)
-
-        # Pressure term of Ohm's law
-        # Notice that we multiply the charge to get the force
-        self.Ex_hat[:] = -self.alpha*1j*self.kx*self.lnrho_hat
-        self.Ey_hat[:] = -self.alpha*1j*self.ky*self.lnrho_hat
-
-        # Transform back to real space
-        E["x"][:-1, :-2] = self.FFT.ifft2(self.Ex_hat, E["x"][:-1, :-2])
-        E["y"][:-1, :-2] = self.FFT.ifft2(self.Ey_hat, E["y"][:-1, :-2])
-
-        # Add resitivity
-        # TODO
+        self.operators.gradient(log(rho), E)
+        E['x'] *= -self.alpha
+        E['y'] *= -self.alpha
