a5bf31bb00d4762cddfe53fc394138d33fbbfeb2
Thomas Berlok <berlok@nbi.dk>
2016-10-21 15:41:23 -0700

    gradient in shearing sheet added to operators

--- skeletor/operators/mpifft4py.py
+++ skeletor/operators/mpifft4py.py
@@ -69,3 +69,81 @@ class Operators:
 
         self.FFT.ifft2(self.fx_hat, grad_inv_del['x'][:-1, :-2])
         self.FFT.ifft2(self.fy_hat, grad_inv_del['y'][:-1, :-2])
+
+
+class ShearOperators(Operators):
+
+    def __init__(self, grid, ax, ay, np):
+
+        from numpy.fft import rfftfreq
+        from numpy import outer, pi, zeros
+        from mpi4py.MPI import COMM_WORLD as comm
+        from skeletor import Complex
+
+        Operators.__init__(self, grid, ax, ay, np)
+
+        # Grid spacing
+        # TODO: this should be a property of the Grid class
+        dx = grid.Lx/grid.nx
+        dy = grid.Ly/grid.ny
+
+        self.temp = zeros((grid.ny/comm.size, grid.nx//2+1), dtype=Complex)
+
+        # Wave numbers for real-to-complex transforms
+        kx_vec = 2*pi*rfftfreq(grid.nx)/dx
+
+        # Outer product of y and kx
+        self.y_kx = outer(grid.y, kx_vec)
+
+        # Maximum value of ky
+        self.ky_max = pi/dy
+
+        # Aspect ratio of grid
+        self.aspect = grid.Lx/grid.Ly
+
+    def _rfft2(self, f, f_hat, phase):
+        from numpy import exp
+        self.FFT.rfftx(f, self.temp)
+        self.temp *= exp(-1j*phase)
+        self.FFT.ffty(self.temp, f_hat)
+
+    def _irfft2(self, f_hat, f, phase):
+        from numpy import exp
+        self.FFT.iffty(f_hat, self.temp)
+        self.temp *= exp(1j*phase)
+        self.FFT.irfftx(self.temp, f)
+
+    def gradient(self, f, grad, St):
+        """Gradient in the shearing sheet using mpifft4py"""
+
+        from numpy import pi, mod
+
+        # We only need to know how much time has elapsed since the last time
+        # the domain was strictly periodic
+        St %= self.aspect
+
+        # Time dependent part of the laboratory frame 'ky'
+        dky = St*self.kx
+
+        # Phase shift to make 'psi' strictly periodic in 'y'.
+        # This is an angle, so it can be mapped into the interval [0, 2*pi)
+        phase = mod(self.y_kx*St, 2*pi)
+
+        self._rfft2(f, self.f_hat, phase)
+
+        # Laboratory frame 'ky'.
+        # Exploit periodicity in Fourier space (i.e. aliasing) and make sure
+        # that  -π/δy ≤ ky < π/δy
+        ky = self.ky + dky
+        ky = mod(ky + self.ky_max, 2*self.ky_max) - self.ky_max
+
+        # Take derivative
+        self.fx_hat[:] = 1j*self.kx*self.f_hat
+        self.fy_hat[:] = 1j*ky*self.f_hat
+
+        # Transform back to real space
+        self._irfft2(self.fx_hat, grad['x'][:-1, :-2], phase)
+        self._irfft2(self.fy_hat, grad['y'][:-1, :-2], phase)
+
+    def grad_inv_del(self, f, grad_inv_del):
+        raise 'grad_inv_del not implemented in shearing sheet'
