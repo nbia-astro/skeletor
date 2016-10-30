1e3476841652544b74a2e707192aaae7bd5a0f2b
Thomas Berlok <berlok@nbi.dk>
2016-10-29 21:31:22 -0700

    Added grid offset in cic push

--- skeletor/cython/push_epicycle.pyx
+++ skeletor/cython/push_epicycle.pyx
@@ -1,38 +1,7 @@
 from ctypes cimport real_t, real2_t, particle_t
 
-def push_epicycle(particle_t[:] particles, real_t dt):
-
-    cdef real_t ey, bz
-
-    cdef real_t vmx, vmy
-    cdef real_t vpx, vpy
-    cdef real_t fac
-
-    cdef real_t Omega = 1.0
-    cdef real_t S = -1.5
-
-    for ip in range(particles.shape[0]):
-
-        bz = Omega*dt
-        ey = -S*particles[ip].y*bz
-
-        vmx = particles[ip].vx
-        vmy = particles[ip].vy + ey
-
-        vpx = vmx + vmy*bz
-        vpy = vmy - vmx*bz
-
-        fac = 2.0/(1.0 + bz*bz)
-
-        particles[ip].vx = vmx + fac*vpy*bz
-        particles[ip].vy = vmy - fac*vpx*bz + ey
-
-        particles[ip].x += particles[ip].vx*dt
-        particles[ip].y += particles[ip].vy*dt
-
-
 def push(particle_t[:] particles, real2_t[:, :] E, real_t bz, real_t qtmh,
-         real_t dt, int noff):
+         real_t dt, int noff, int lbx, int lby):
 
     cdef real_t ex, ey
 
@@ -61,6 +30,9 @@ def push(particle_t[:] particles, real2_t[:, :] E, real_t bz, real_t qtmh,
         ty = 1.0 - dy
 
         iy -= noff
+
+        ix += lbx
+        iy += lby
 
         ex = dy*(dx*E[iy+1, ix+1].x + tx*E[iy+1, ix].x)  \
             + ty*(dx*E[iy, ix+1].x + tx*E[iy, ix].x)
