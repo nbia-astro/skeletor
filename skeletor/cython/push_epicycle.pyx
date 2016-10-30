431ed5e77ff881cd28c73e5fb77225a5f4474866
Thomas Berlok <berlok@nbi.dk>
2016-10-29 18:44:21 -0700

    TSC interpolation in push and deposition

--- skeletor/cython/push_epicycle.pyx
+++ skeletor/cython/push_epicycle.pyx
@@ -1,6 +1,6 @@
 from ctypes cimport real_t, real2_t, particle_t
 
-def push(particle_t[:] particles, real2_t[:, :] E, real_t bz, real_t qtmh,
+def push_cic(particle_t[:] particles, real2_t[:, :] E, real_t bz, real_t qtmh,
          real_t dt, int noff, int lbx, int lby):
 
     cdef real_t ex, ey
@@ -39,6 +39,75 @@ def push(particle_t[:] particles, real2_t[:, :] E, real_t bz, real_t qtmh,
 
         ey = dy*(dx*E[iy+1, ix+1].y + tx*E[iy+1, ix].y)  \
             + ty*(dx*E[iy, ix+1].y + tx*E[iy, ix].y)
+
+        # Rescale electric & magnetic field with qtmh = 0.5*dt*charge/mass
+        ex *= qtmh
+        ey *= qtmh
+        bz *= qtmh
+
+        vmx = particles[ip].vx + ex
+        vmy = particles[ip].vy + ey
+
+        vpx = vmx + vmy*bz
+        vpy = vmy - vmx*bz
+
+        fac = 2.0/(1.0 + bz*bz)
+
+        particles[ip].vx = vmx + fac*vpy*bz + ex
+        particles[ip].vy = vmy - fac*vpx*bz + ey
+
+        particles[ip].x += particles[ip].vx*dt
+        particles[ip].y += particles[ip].vy*dt
+
+def push_tsc(particle_t[:] particles, real2_t[:, :] E, real_t bz, real_t qtmh,
+         real_t dt, int noff, int lbx, int lby):
+
+    cdef real_t ex, ey
+
+    cdef real_t vmx, vmy
+    cdef real_t vpx, vpy
+    cdef real_t fac
+
+    cdef int ix, iy
+    cdef real_t x, y
+    cdef real_t dx, dy
+    cdef real_t wmx, w0x, wpx, wmy, w0y, wpy
+
+    for ip in range(particles.shape[0]):
+
+        # Interpolate field onto particle (TODO: Move to separate function)
+        x = particles[ip].x
+        y = particles[ip].y
+
+        x += 0.5
+        y += 0.5
+
+        ix = <int> x
+        iy = <int> y
+
+        dx = x - <real_t> ix - 0.5
+        dy = y - <real_t> iy - 0.5
+
+        w0x = 0.75 - dx*dx
+        wpx = 0.5*(0.5 + dx)**2
+        wmx = 1.0 - (w0x + wpx)
+
+        w0y = 0.75 - dy*dy
+        wpy = 0.5*(0.5 + dy)**2
+        wmy = 1.0 - (w0y + wpy)
+
+        iy -= noff
+
+        ix += lbx
+        iy += lby
+
+        ex = wmy*(wmx*E[iy-1, ix-1].x+w0x*E[iy-1, ix  ].x+wpx*E[iy-1, ix+1].x)\
+           + w0y*(wmx*E[iy  , ix-1].x+w0x*E[iy  , ix  ].x+wpx*E[iy  , ix+1].x)\
+           + wpy*(wmx*E[iy+1, ix-1].x+w0x*E[iy+1, ix  ].x+wpx*E[iy+1, ix+1].x)
+
+        ey = wmy*(wmx*E[iy-1, ix-1].y+w0x*E[iy-1, ix  ].y+wpx*E[iy-1, ix+1].y)\
+           + w0y*(wmx*E[iy  , ix-1].y+w0x*E[iy  , ix  ].y+wpx*E[iy  , ix+1].y)\
+           + wpy*(wmx*E[iy+1, ix-1].y+w0x*E[iy+1, ix  ].y+wpx*E[iy+1, ix+1].y)
 
         # Rescale electric & magnetic field with qtmh = 0.5*dt*charge/mass
         ex *= qtmh
--- skeletor/sources.py
+++ skeletor/sources.py
@@ -1,20 +1,39 @@
 from .field import Field
-from .cython.deposit import deposit as cython_deposit
 from .cython.ppic2_wrapper import cppgpost2l
 
 
 class Sources:
 
-    def __init__(self, grid, **kwds):
+    def __init__(self, grid, order='cic', **kwds):
 
         self.rho = Field(grid, **kwds)
 
+        # Interpolation order
+        self.order = order
+
     def deposit(self, particles, erase=True):
+        if self.order == 'cic':
+            from .cython.deposit import deposit_cic as cython_deposit
+        elif self.order == 'tsc':
+            from .cython.deposit import deposit_tsc as cython_deposit
+        else:
+            msg = 'Interpolation order not supported. order = {}'
+            raise RuntimeError(msg.format(self.order))
 
         if erase:
             self.rho.fill(0.0)
 
         cython_deposit(
+                particles[:particles.np], self.rho, particles.charge,
+                self.rho.grid.noff, self.rho.grid.lbx, self.rho.grid.lby)
+
+    def deposit_tsc(self, particles, erase=True):
+        from .cython.deposit import deposit_tsc
+
+        if erase:
+            self.rho.fill(0.0)
+
+        deposit_tsc(
                 particles[:particles.np], self.rho, particles.charge,
                 self.rho.grid.noff, self.rho.grid.lbx, self.rho.grid.lby)
 
