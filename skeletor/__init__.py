d35ced4c1bccacc92d1a9e1d8956f11549f81626
Thomas Berlok <berlok@nbi.dk>
2016-12-01 11:51:59 +0100

    Added a simple Faraday's law

--- skeletor/__init__.py
+++ skeletor/__init__.py
@@ -6,6 +6,7 @@ from .particles import Particles
 from .particle_sort import ParticleSort
 from .poisson import Poisson
 from .ohm import Ohm
+from .faraday import Faraday
 from .sources import Sources
 from .initial_condition import uniform_density, velocity_perturbation
 from .initial_condition import InitialCondition
