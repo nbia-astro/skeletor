65c9a3bd4cf69eb1ecdaf2a474e8b68003ac615e
Thomas Berlok <berlok@nbi.dk>
2016-12-01 11:50:57 +0100

    Ohm's law now also has the J term

--- skeletor/ohm.py
+++ skeletor/ohm.py
@@ -14,14 +14,35 @@ class Ohm:
         # Resistivity
         self.eta = eta
 
+        self.npc = npc
+
     @property
     def alpha(self):
         # Ratio of temperature to charge
         return self.temperature/self.charge
 
-    def __call__(self, rho, E, destroy_input=True):
+    def __call__(self, rho, E, B, J, destroy_input=True):
         from skeletor import log
+        from numpy import empty_like
 
-        self.operators.gradient(log(rho), E)
+        logrho = empty_like(rho)
+        logrho = log(rho)
+        logrho.copy_guards()
+
+        self.operators.gradient(logrho, E)
         E['x'] *= -self.alpha
         E['y'] *= -self.alpha
+
+        Je = empty_like(J)
+
+        # Ampere's law to calculate total current
+        self.operators.curl(B, Je)
+
+        # Subtract ion current to get electron current
+        Je['x'] -= J['x']/self.npc
+        Je['y'] -= J['y']/self.npc
+        Je['z'] -= J['z']/self.npc
+
+        E['x'] += Je['y']*B['z'] - Je['z']*B['y']
+        E['y'] += Je['z']*B['x'] - Je['x']*B['z']
+        E['z'] += Je['x']*B['y'] - Je['y']*B['x']
