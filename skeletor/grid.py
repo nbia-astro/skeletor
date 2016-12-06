cc86bf9962b760bb0babb76c4245272645d5d2d5
Thomas Berlok <berlok@nbi.dk>
2016-12-06 15:50:13 +0100

    Revert "Restructured 'Grid' class"
    This reverts commit 02d0af7d0b1a3ad5487443c1d229fcc485c529c8.

--- skeletor/grid.py
+++ skeletor/grid.py
@@ -1,16 +1,36 @@
 from .cython.ppic2_wrapper import grid_t
-from .domain import SubDomain
 
 
-class Grid(SubDomain, grid_t):
-    """
-    This class defines a differentiable (sub-) manifold.
-    """
+class Grid(grid_t):
 
     def __init__(self, nx, ny, comm, nlbx=0, nubx=2, nlby=0, nuby=1):
 
-        # Initialize SubDomain class
-        super().__init__(nx, ny, comm)
+        from .cython.ppic2_wrapper import cppinit
+        from numpy import array
+
+        # Number of grid points in x- and y-direction
+        self.nx = nx
+        self.ny = ny
+        # MPI communicator
+        self.comm = comm
+
+        # Start parallel processing. Calling this function necessary even
+        # though `MPI.Init()` has already been called by importing `MPI` from
+        # `mpi4py`. The reason is that `cppinit()` sets a number of global
+        # variables in the C library source file (`ppic2/pplib2.c`). The
+        # returned variables `idproc` and `nvp` are simply the MPI rank (i.e.
+        # processor id) and size (i.e. total number of processes),
+        # respectively.
+        idproc, nvp = cppinit(comm)
+
+        # nyp = number of primary (complete) gridpoints in particle partition
+        self.nyp = ny//comm.size
+
+        # noff = lowermost global gridpoint in particle partition
+        self.noff = self.nyp*comm.rank
+
+        # edges[0:1] = lower:upper boundary of particle partition
+        self.edges = array([self.noff, self.noff + self.nyp])
 
         # Ghost zone setup
         # nlbx, nlby = number of ghost zones at lower boundary in x, y
@@ -27,9 +47,49 @@ class Grid(SubDomain, grid_t):
         self.lby = nlby
         self.uby = self.nyp + self.nlby
 
+        # Parameters needed by PPIC2
+        self.kstrt = comm.rank + 1
+        self.nvp = comm.size
+
         # nypmx = size of particle partition, including guard cells, in y
         # nxpmx = size of particle partition, including guard cells, in x
         # nypmn = value of nyp
         self.nypmx = self.nyp + self.nlby + self.nuby
         self.nypmn = self.nyp
         self.nxpmx = self.nx + self.nlbx + self.nubx
+
+        if comm.size > self.ny:
+            msg = "Too many processors requested: ny={}, comm.size={}"
+            raise RuntimeError(msg.format(self.ny, comm.size))
+
+    @property
+    def Lx(self):
+        "Domain size in x"
+        # This is defined as a property because right now,
+        # Lx must be equal to nx.
+        return self.nx
+
+    @property
+    def Ly(self):
+        "Domain size in y"
+        # This is defined as a property because right now,
+        # Ly must be equal to ny.
+        return self.ny
+
+    @property
+    def x(self):
+        "One-dimensional x-coordinate array"
+        from numpy import arange
+        return arange(self.nx)
+
+    @property
+    def y(self):
+        "One-dimensional y-coordinate array"
+        from numpy import arange
+        return arange(self.noff, self.noff + self.nyp)
+
+    @property
+    def yg(self):
+        "One-dimensional y-coordinate array including ghost"
+        from numpy import arange
+        return arange(self.noff, self.noff + self.nyp + 1)
