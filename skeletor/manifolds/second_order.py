465894afa78e062038fa784f6059dadb60dea058
Tobias Heinemann <tobias.heinemann@gmail.com>
2016-12-11 02:10:27 +0100

    Changed default number of guard layers
    ... for second-order finite-difference manifold. The default is now one
    guard layer on each boundary (so four in total), which is the minimum
    number required for central second-order finite differences.

--- skeletor/manifolds/second_order.py
+++ skeletor/manifolds/second_order.py
@@ -7,7 +7,7 @@ class Manifold(Grid):
 
     def __init__(
             self, nx, ny, comm,
-            ax=0.0, ay=0.0, nlbx=0, nubx=2, nlby=0, nuby=1):
+            ax=0.0, ay=0.0, nlbx=1, nubx=1, nlby=1, nuby=1):
 
         super().__init__(
                 nx, ny, comm, nlbx=nlbx, nubx=nubx, nlby=nlby, nuby=nuby)
