e11d22029be554d4c48758a95dbb739d6a40f831
Thomas Berlok <berlok@nbi.dk>
2017-01-10 17:23:20 +0100

    Cleaned up sheared Burgers' widget
    For some reason nothing happens when I run this
    with MPI. Not that important for a widget, I guess.

--- example/sheared_burgers_widget.py
+++ example/sheared_burgers_widget.py
@@ -1,5 +1,11 @@
-from skeletor import cppinit, Float, Float2, Particles, Sources
-from skeletor import ShearField
+"""
+This script creates a widget comparing the density field for a sheared
+disturbance with the analytical solution. This disturbance correponds to
+the standard 1D Burgers' equation in the primed coordinate, x' = x + Sty. See
+also test_burgers.py.
+"""
+
+from skeletor import cppinit, Float, Float2, Particles, Sources, ShearField
 from skeletor.manifolds.second_order import ShearingManifold
 import numpy
 from mpi4py import MPI
@@ -43,31 +49,8 @@ a = a.flatten()
 b = b.flatten()
 
 
-def vx_an(ap, bp, t):
-    vx = ampl*numpy.sin(kx*ap) - bp*S
-    return vx
-
-
-def vy_an(ap, bp, t):
-    vy = numpy.zeros_like(ap)
-    return vy
-
-
-def x_an(ap, bp, t):
-
-    return ap + vx_an(ap, bp, t)*t
-
-
-def y_an(ap, bp, t):
-    return bp
-
-
-def rho_an(a, t):
-    return 1/(1 + ampl*kx*numpy.cos(kx*a)*t)
-
-
 def mean(f, axis=None):
-    "Compute mean of an array across processors."
+    """Compute mean of an array across processors."""
     result = numpy.mean(f, axis=axis)
     if axis is None or axis == 0:
         # If the mean is to be taken over *all* axes or just the y-axis,
@@ -77,49 +60,50 @@ def mean(f, axis=None):
 
 
 def rms(f):
-    "Compute root-mean-square of an array across processors."
+    """Compute root-mean-square of an array across processors."""
     return numpy.sqrt(mean(f**2))
 
 
 def velocity(a):
-    "Particle velocity in Lagrangian coordinates."
+    """Particle velocity in Lagrangian coordinates."""
     return ampl*numpy.sin(kx*a)
 
 
 def velocity_prime(a):
-    "Derivative of particle velocity in Lagrangian coordinates: ∂v(a,τ)/∂a"
+    """Derivative of particle velocity in Lagrangian coordinates:
+    ∂v(a,τ)/∂a"""
     return ampl*kx*numpy.cos(kx*a)
 
 
 def euler(a, τ):
     """
-    This function converts from Lagrangian to Eulerian coordinate by
-    solving ∂x(a, τ)/∂τ = u(a) for x(a, τ) subject to the initial condition
-    x(a, 0) = a.
+    This function converts from Lagrangian to Eulerian sheared coordinate x'
+    by solving ∂x'(a, τ)/∂τ = U(a) for x'(a, τ) subject to the initial
+    condition x'(a, 0) = a.
     """
     return (a + velocity(a)*τ) % nx
 
 
 def euler_prime(a, τ):
     """
-    The derivative ∂x/∂a of the conversion function defined above, which is
+    The derivative ∂x'/∂a of the conversion function defined above, which is
     related to the mass density in Lagrangian coordinates through
-        rho(a, τ)/rho_0(a) = (∂x/∂a)⁻¹,
-    where rho_0(a) = rho(a, 0) is the initial mass density.
+    rho(a, τ)/rho_0(a) = (∂x'/∂a)⁻¹, where rho_0(a) = rho(a, 0) is the initial
+    mass density.
     """
     return 1 + velocity_prime(a)*τ
 
 
-def lagrange(x, t, tol=1.48e-8, maxiter=50):
+def lagrange(xp, t, tol=1.48e-8, maxiter=50):
     """
-    Given the Eulerian coordinate x and time t, this function solves the
-    definition x = euler(a, t) for the Lagrangian coordinate a via the
-    Newton-Raphson method.
+    Given the Eulerian coordinate x' = x + Sty and time t, this function
+    solves the definition x' = euler(a, t) for the Lagrangian coordinate a via
+    the Newton-Raphson method.
     """
     # Use Eulerian coordinate as initial guess
-    a = x.copy()
+    a = xp.copy()
     for it in range(maxiter):
-        f = euler(a, t) - x
+        f = euler(a, t) - xp
         df = euler_prime(a, t)
         b = a - f/df
         # This is not the safest criterion, but seems good enough
@@ -128,15 +112,35 @@ def lagrange(x, t, tol=1.48e-8, maxiter=50):
         a = b.copy()
 
 
+def rho_an(a, t):
+    """
+    Calculates the analytical density as a function of the lagragian
+    coordiate, a.
+    """
+    return 1/euler_prime(a, t)
+
+
 def rho2d_an(x, y, t):
+    """
+    Calculate the analytical density as a function of Eulerian grid position,
+    (x, y), and time, t. Accepts 2D arrays for x and y.
+    """
     xp = x + S*y*t
     xp %= nx
     a = lagrange(xp, t)
     return rho_an(a, t)
 
 
-# Phase
-phi = kx*a
+def vx_an(a, b, t):
+    """Particle velocity along x is perturbation plus shear"""
+    vx = velocity(a) - b*S
+    return vx
+
+
+def x_an(a, b, t):
+    """Particle x-position as a function of time"""
+    return a + vx_an(a, b, t)*t
+
 
 # Particle positions at time= 0
 x = a
@@ -187,6 +191,9 @@ assert numpy.isclose(comm.allreduce(
     sources.rho.trim().sum(), op=MPI.SUM), np*charge)
 sources.rho.copy_guards()
 
+# Copy density into a shear field
+rho_periodic.active = sources.rho.trim()
+
 
 def concatenate(arr):
     """Concatenate local arrays to obtain global arrays
@@ -195,11 +202,21 @@ def concatenate(arr):
 
 
 def update(t):
+    """
+    This function updates the particle position and velocity according to the
+    analytical solution, deposits the charge and current, and produces two
+    figures. Figure 1 shows rho and Jx, which are shearing periodic, in
+    the first column. In the second column it shows the same fields but
+    translated by a distance -S*t*y along x. This makes the fields periodic.
+
+    The second figures then shows the analytical solutions for rho and Jx
+    as a function of the primed coordinate, x', which are compared with the
+    numerical solution found by averaging the periodic fields shown in Figure
+    1 along y.
+    """
 
     ions['x'][:np] = x_an(a, b, t)
-    ions['y'][:np] = y_an(a, b, t)
     ions['vx'][:np] = vx_an(a, b, t)
-    ions['vy'][:np] = vy_an(a, b, t)
     ions.time = t
     ions.shear_periodic_y()
     ions.periodic_x()
@@ -233,66 +250,58 @@ def update(t):
     J_periodic.copy_guards()
 
     err = rms(sources.rho.trim()/npc - rho2d_an(xx, yy, t))
-    print(err)
 
     global_rho = concatenate(sources.rho.trim())
     global_rho_periodic = concatenate(rho_periodic.trim())
     global_J = concatenate(sources.J.trim())
     global_J_periodic = concatenate(J_periodic.trim())
+
     if comm.rank == 0:
+        # Update 2D images
         im1a.set_data(global_rho)
         im2a.set_data(global_rho_periodic)
         im1b.set_data(global_J['x']/global_rho)
         im2b.set_data(global_J_periodic['x']/global_rho_periodic)
-        im1c.set_data(global_J['y']/global_rho)
-        im2c.set_data(global_J_periodic['y']/global_rho_periodic)
-        im1a.autoscale()
-        im2a.autoscale()
-        im1b.autoscale()
-        im2b.autoscale()
-        im1c.autoscale()
-        im2c.autoscale()
+        for im in (im1a, im2a, im1b, im2b):
+            im.autoscale()
+        # Update 1D solutions (numerical and analytical)
         im4[0].set_ydata(global_rho_periodic.mean(axis=0)/npc)
         im5[0].set_ydata((global_J_periodic['x']/global_rho_periodic).
                          mean(axis=0))
-        im6[0].set_ydata((global_J_periodic['y']/global_rho_periodic).
-                         mean(axis=0))
-        xp_par = vx_an(manifold.x, 0, t)*t + manifold.x
-        xp_par %= nx
-        im4[1].set_data(xp_par, rho_an(manifold.x, t))
-        im6[1].set_data(xp_par, vy_an(manifold.x, 0, t))
-        im5[1].set_data(xp_par, vx_an(manifold.x, 0, t))
+        xp = euler(manifold.x, t)
+        im4[1].set_data(xp, rho_an(manifold.x, t))
+        im5[1].set_data(xp, velocity(manifold.x))
 
+        print(err)
+
+
+global_rho = concatenate(sources.rho.trim())
+global_rho_periodic = concatenate(rho_periodic.trim())
+global_J = concatenate(sources.J.trim())
+global_J_periodic = concatenate(J_periodic.trim())
 
 if comm.rank == 0:
-    global_rho = concatenate(sources.rho.trim())
-    global_rho_periodic = concatenate(rho_periodic.trim())
-    global_J = concatenate(sources.J.trim())
-    global_J_periodic = concatenate(J_periodic.trim())
 
     plt.rc('image', origin='upper', interpolation='nearest',
            cmap='coolwarm')
     plt.figure(1)
     plt.clf()
-    fig, axes = plt.subplots(num=1, ncols=2, nrows=3)
+    fig, axes = plt.subplots(num=1, ncols=2, nrows=2)
     im1a = axes[0, 0].imshow(global_rho)
     im2a = axes[0, 1].imshow(global_rho_periodic)
     im1b = axes[1, 0].imshow(global_J['x']/global_rho)
     im2b = axes[1, 1].imshow(global_J_periodic['x']/global_rho_periodic)
-    im1c = axes[2, 0].imshow(global_J['y']/global_rho)
-    im2c = axes[2, 1].imshow(global_J_periodic['y']/global_rho_periodic)
     axtime1 = plt.axes([0.125, 0.1, 0.775, 0.03])
     stime1 = mw.Slider(axtime1, 'Time', -numpy.pi, numpy.pi/2, 0)
     stime1.on_changed(update)
 
     plt.figure(2)
     plt.clf()
-    fig2, (ax1, ax2, ax3) = plt.subplots(num=2, nrows=3)
+    fig2, (ax1, ax2) = plt.subplots(num=2, nrows=2)
     plt.subplots_adjust(bottom=0.25)
     ax1.set_ylim(0, 2)
     ax2.set_ylim(-1*ampl, 1*ampl)
-    ax3.set_ylim(-2*ampl, 2*ampl)
-    for ax in (ax1, ax2, ax3):
+    for ax in (ax1, ax2):
         ax.set_xlim(0, nx)
     ax1.set_xlabel(r"$x'$")
     ax1.set_title(r'$\rho/\rho_0$')
@@ -300,9 +309,8 @@ if comm.rank == 0:
     axtime2 = plt.axes([0.125, 0.1, 0.775, 0.03])
     stime2 = mw.Slider(axtime2, 'Time', -numpy.pi, numpy.pi/2, 0)
     stime2.on_changed(update)
-    xp_par = x_an(a, b, 0) + S*y_an(a, b, 0)*0
-    xp_par %= nx
-    xp_par = numpy.sort(xp_par)
+    xp = euler(manifold.x, 0)
+    xp = numpy.sort(xp)
     im4 = ax1.plot(manifold.x, (global_rho_periodic.mean(axis=0))/npc,
                    'b',
                    manifold.x, (global_rho_periodic.mean(axis=0))/npc,
@@ -310,10 +318,6 @@ if comm.rank == 0:
     im5 = ax2.plot(manifold.x, (global_J_periodic['x']/global_rho_periodic)
                    .mean(axis=0), 'b',
                    manifold.x, (global_J_periodic['x']/global_rho_periodic)
-                   .mean(axis=0), 'r--')
-    im6 = ax3.plot(manifold.x, (global_J_periodic['y']/global_rho_periodic)
-                   .mean(axis=0), 'b',
-                   manifold.x, (global_J_periodic['y']/global_rho_periodic)
                    .mean(axis=0), 'r--')
     update(0)
     plt.show()
