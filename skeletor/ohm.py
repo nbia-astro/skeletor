cedb4c146bb512622a777a38c5e0a2c317475db5
Thomas Berlok <berlok@nbi.dk>
2017-01-31 17:09:29 +0100

    Added Je x B to Ohm's law

--- skeletor/ohm.py
+++ skeletor/ohm.py
@@ -2,11 +2,13 @@ class Ohm:
 
     """Solve Ohm's law"""
 
-    def __init__(self, manifold, charge=1.0, temperature=0.0, eta=0.0):
+    def __init__(self, manifold, charge=1.0, temperature=0.0, eta=0.0,
+                 npc=None):
 
         # Store the operators here for easy access
         self.gradient = manifold.gradient
         self.log = manifold.log
+        self.curl = manifold.curl
 
         # Charge
         self.charge = charge
@@ -15,15 +17,36 @@ class Ohm:
         # Resistivity
         self.eta = eta
 
+        self.npc = npc
+
     @property
     def alpha(self):
         # Ratio of temperature to charge
         return self.temperature/self.charge
 
-    def __call__(self, rho, E):
+    def __call__(self, rho, E, B, J):
+        from numpy import empty_like
 
         self.gradient(self.log(rho), E)
-        E['x'] *= -self.alpha/E.grid.dx
-        E['y'] *= -self.alpha/E.grid.dy
+        E['x'] *= -self.alpha
+        E['y'] *= -self.alpha
 
+        Je = empty_like(J)
+
+        # Ampere's law to calculate total current
+        self.curl(B, Je)
+
+        # Subtract ion current to get electron current
+        Je['x'] -= J['x']/self.npc
+        Je['y'] -= J['y']/self.npc
+        Je['z'] -= J['z']/self.npc
+
+        E['x'] += Je['y']*B['z'] - Je['z']*B['y']
+        E['y'] += Je['z']*B['x'] - Je['x']*B['z']
+        E['z'] += Je['x']*B['y'] - Je['y']*B['x']
+
+        # Convert to dimensionless units (Hardcoded to cubic cells!)
+        E['x'] /= E.grid.dx
+        E['y'] /= E.grid.dy
+        E['z'] /= E.grid.dx
 
