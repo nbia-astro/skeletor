dc3a6929bb4575cc19660c1b88cb75a5d46f78dc
Thomas Berlok <berlok@nbi.dk>
2017-03-24 13:29:24 +0100

    First attempt at parallel firehose

--- example/firehose.py
+++ example/firehose.py
@@ -3,20 +3,25 @@ from skeletor import Ohm, Faraday, State
 from skeletor.manifolds.second_order import Manifold
 from skeletor.time_steppers.horowitz import TimeStepper
 import numpy as np
+from numpy.random import normal
 from mpi4py import MPI
 from mpi4py.MPI import COMM_WORLD as comm
 from scipy.special import erfinv
 
-plot = True
+fitplot = True
+plot = False
 # Quiet start
 quiet = True
 # Number of grid points in x- and y-direction
-nx, ny = 64, 64
+nx, ny = 32, 1
 # Grid size in x- and y-direction (square cells!)
-Lx = nx
-Ly = ny
+Lx, Ly = 13.20370041, 13.20370041
+
+# Grid distances
+dx, dy = Lx/nx, Ly/ny
+
 # Average number of particles per cell
-npc = 64
+npc = 2**10
 # Particle charge and mass
 charge = 1.0
 mass = 1.0
@@ -27,14 +32,14 @@ A = 0.005
 # Wavenumbers
 ikx = 1
 iky = 0
-ampl= 1e-6
+ampl = 1e-6
 # Thermal velocity of electrons in x- and y-direction
 
 # Magnetic field strength
 B0 = 1
 va = B0
-beta_para = 2000
-beta_perp = 500
+beta_para = 3
+beta_perp = 0.8
 vtx = np.sqrt(va**2*beta_para/2)
 vty = np.sqrt(va**2*beta_perp/2)
 vtz = vty
@@ -56,7 +61,7 @@ theta = np.arctan(iky/ikx) if ikx != 0 else np.pi/2
 (Bx, By, Bz) = (B0*np.cos(theta), B0*np.sin(theta), 0)
 
 # Simulation time
-tend = 10
+tend = 50
 
 # Uniform distribution of particle positions (quiet start)
 sqrt_npc = int(np.sqrt(npc))
@@ -66,31 +71,43 @@ x_cell, y_cell = np.meshgrid(a, a)
 x_cell = x_cell.flatten()
 y_cell = y_cell.flatten()
 
-R = (np.arange(npc) + 0.5)/npc
-vx_cell = erfinv(2*R - 1)*np.sqrt(2)*vtx
-vy_cell = erfinv(2*R - 1)*np.sqrt(2)*vty
-vz_cell = erfinv(2*R - 1)*np.sqrt(2)*vtz
-np.random.shuffle(vx_cell)
-np.random.shuffle(vy_cell)
-np.random.shuffle(vz_cell)
-for i in range(nx):
-    for j in range(ny):
-        if i == 0 and j == 0:
-            x = x_cell + i
-            y = y_cell + j
-            vx = vx_cell
-            vy = vy_cell
-            vz = vz_cell
-        else:
-            x = np.concatenate((x, x_cell + i))
-            y = np.concatenate((y, y_cell + j))
-            vx = np.concatenate((vx, vx_cell))
-            vy = np.concatenate((vy, vy_cell))
-            vz = np.concatenate((vz, vz_cell))
+if comm.rank == 0:
+    R = (np.arange(npc) + 0.5)/npc
+    vx_cell = erfinv(2*R - 1)*np.sqrt(2)*vtx
+    vy_cell = erfinv(2*R - 1)*np.sqrt(2)*vty
+    vz_cell = erfinv(2*R - 1)*np.sqrt(2)*vtz
+    np.random.shuffle(vx_cell)
+    np.random.shuffle(vy_cell)
+    np.random.shuffle(vz_cell)
+    for i in range(nx):
+        for j in range(ny):
+            if i == 0 and j == 0:
+                x = x_cell + i
+                y = y_cell + j
+                vx = vx_cell
+                vy = vy_cell
+                vz = vz_cell
+            else:
+                x = np.concatenate((x, x_cell + i))
+                y = np.concatenate((y, y_cell + j))
+                vx = np.concatenate((vx, vx_cell))
+                vy = np.concatenate((vy, vy_cell))
+                vz = np.concatenate((vz, vz_cell))
+else:
+    x, y, vx, vy, vz = None, None, None, None, None
+
+x = comm.bcast(x, root=0)
+y = comm.bcast(y, root=0)
+vx = comm.bcast(vx, root=0)
+vy = comm.bcast(vy, root=0)
+vz = comm.bcast(vz, root=0)
+
+x *= dx
+y *= dy
 
 # Create numerical grid. This contains information about the extent of
 # the subdomain assigned to each processor.
-manifold = Manifold(nx, ny, comm, Lx=nx, Ly=ny, lbx=2, lby=2)
+manifold = Manifold(nx, ny, comm, Lx=Lx, Ly=Ly, lbx=2, lby=2)
 
 # Time step
 dt = 1e-2
@@ -112,16 +129,7 @@ ions = Particles(manifold, Nmax, charge=charge, mass=mass)
 # Assign particles to subdomains
 ions.initialize(x, y, vx, vy, vz)
 
-# Create a uniform density field
-# init = InitialCondition(npc, quiet=quiet)
-# init(manifold, ions)
-
-# Perturbation to particle velocities
-from np.random import normal
-# ions['vx'][:ions.N] = vtx*normal(size=ions.N)
-# ions['vy'][:ions.N] = vty*normal(size=ions.N)
-# ions['vz'][:ions.N] = vtz*normal(size=ions.N)
-
+# Perturbation to magnetic field
 # Add background magnetic field
 B = Field(manifold, dtype=Float3)
 B.fill((Bx, By, Bz))
@@ -143,14 +151,17 @@ e = TimeStepper(state, ohm, manifold)
 # Deposit charges and calculate initial electric field
 e.prepare(dt)
 
+
 # Concatenate local arrays to obtain global arrays
 # The result is available on all processors.
 def concatenate(arr):
     return np.concatenate(comm.allgather(arr))
 
+if plot or fitplot:
+    import matplotlib.pyplot as plt
+
 # Make initial figure
 if plot:
-    import matplotlib.pyplot as plt
     from matplotlib.cbook import mplDeprecation
     import warnings
 
@@ -201,15 +212,42 @@ for it in range(nt):
                             "ignore", category=mplDeprecation)
                     plt.pause(1e-7)
 if comm.rank == 0:
-    if plot:
+    if fitplot:
+
         Bx_mag = np.array(Bx_mag)
         By_mag = np.array(By_mag)
         By_mag = np.array(By_mag)
+        B_mag = Bx_mag + By_mag + Bz_mag
         time = np.array(time)
+
+        # TODO: Solve this here!
+        gamma_t = 0.1566
+
+        from scipy.optimize import curve_fit
+
+        # Exponential growth function
+        def func(x, a, b):
+            return a*np.exp(b*x)
+
+        def lin_func(x, a, b):
+            return a + b*x
+
+        # Fit exponential to the evolution of sqrt(mean(B_x**2))
+        # Disregard first half of data
+        first = int(0.34*nt/20)
+        last = int(0.6*nt/20)
+        popt, pcov = curve_fit(lin_func, time[first:last],
+                               np.log(B_mag[first:last]))
         plt.figure(2)
         plt.semilogy(time, Bx_mag, label=r"$(\delta B_x)^2$")
         plt.semilogy(time, By_mag, label=r"$(\delta B_y)^2$")
         plt.semilogy(time, Bz_mag, label=r"$(\delta B_z)^2$")
+        plt.semilogy(time, B_mag, label=r"$(\delta B)^2$")
+        gamma_f = popt[1]/2
+        plt.semilogy(time, func(time, 1e-12, popt[1]),
+                     label=r"Fit: $\gamma = %.5f$" % gamma_f)
+        plt.semilogy(time, func(time, 1e-12, gamma_t), 'k-',
+                     label=r"Theory: $\gamma = %.5f$" % gamma_t)
         plt.legend(frameon=False, loc=2)
         plt.xlabel(r"$t$")
         plt.show()
