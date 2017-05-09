3dd063f20a1fc6d8b48a2d8e9a460c8f756cb9a5
Thomas Berlok <berlok@nbi.dk>
2017-05-09 15:56:12 +0200

    state.py converts particle object to tuple
    this is a bit of a hack...

--- skeletor/state.py
+++ skeletor/state.py
@@ -1,6 +1,17 @@
 class State:
 
     def __init__(self, species, B, time=0.0):
-        self.species = species
+        """
+        State class.
+        Inputs:
+        species: tuple of particle objects (ions1, ions2, ions3) or
+        a single particle object. In the latter case the particle object is
+        still stored as a tuple.
+        """
+        if type(species) == tuple:
+            self.species = species
+        else:
+            # Convert single particle array to a tuple with one element.
+            self.species = (species,)
         self.B = B
         self.t = time
