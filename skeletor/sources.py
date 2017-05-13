89cafdae4eabd66ce2a185c43eed284ce043b141
Tobias Heinemann <tobias.heinemann@gmail.com>
2017-05-13 15:09:19 +0200

    Moved add_guards() from Field to Sources

--- skeletor/sources.py
+++ skeletor/sources.py
@@ -61,3 +61,83 @@ class Sources(Field):
         self.add_guards()
         # Copy guards
         self.copy_guards()
+
+    def __iadd__(self, other):
+        """Overloads the += operator. This is convenient for doing arithmetic
+        with structured Numpy arrays (in particular the sources array)."""
+
+        # If this is not a structured array, just call the corresponding method
+        # from ndarray. Note that we can't replace the call to __iadd__ with
+        #   self += other
+        # as that would lead to infinite recursion
+        if self.dtype.names is None:
+            return super().__iadd__(other)
+
+        # If this _is_ a structured array, loop over all names
+        for dim in self.dtype.names:
+            self[dim] += other[dim]
+        # NOTE: The following would be more efficient, but is not as generic
+        # because it only works for homogeneous floating point types.
+        #   self.view(Float) += other.view(Float)
+        return self
+
+    def add_guards_x(self):
+        """
+        Add data from guard cells in x to corresponding active cells.
+        Note: this acts on _all_ grid points along y (active plus guards).
+        """
+        g = self.grid
+
+        for ix in range(g.lbx):
+            self[:, ix + g.nx] += self[:, ix]
+        for ix in range(g.ubx + g.lbx - 1, g.ubx - 1, -1):
+            self[:, ix - g.nx] += self[:, ix]
+
+    def add_guards_y(self):
+        """
+        Add data from guard cells in y to corresponding active cells.
+        Note: this only acts on active grid points along x.
+        """
+        g = self.grid
+
+        self[g.uby:, g.lbx:g.ubx] = self.send_up(self[g.uby:, g.lbx:g.ubx])
+        self[:g.lby, g.lbx:g.ubx] = self.send_dn(self[:g.lby, g.lbx:g.ubx])
+        for iy in range(g.lby):
+            self[iy + g.nyp, g.lbx:g.ubx] += self[iy, g.lbx:g.ubx]
+        for iy in range(g.uby + g.lby - 1, g.uby - 1, -1):
+            self[iy - g.nyp, g.lbx:g.ubx] += self[iy, g.lbx:g.ubx]
+
+    def add_guards(self):
+        "Add data from guard cells to corresponding active cells."
+
+        # TODO: We might wanna do some kind of check analogous to the one being
+        # done in `copy_guards()`. And here we should probably throw an error
+        # if it fails because calling this method multiple times actually does
+        # cause harm.
+
+        # x-boundaries
+        self.add_guards_x()
+
+        if self.shear:
+            # Short hand
+            g = self.grid
+            # Translate the y-ghostzones
+            if g.comm.rank == g.comm.size - 1:
+                trans = g.Ly*g.S*self.time
+                for iy in range(g.uby, g.uby + g.lby):
+                    self._translate_boundary(trans, iy)
+            if g.comm.rank == 0:
+                trans = -g.Ly*g.S*self.time
+                for iy in range(0, g.lby):
+                    self._translate_boundary(trans, iy)
+
+        # y-boundaries
+        self.add_guards_y()
+
+        # Erase guard cells
+        # TODO: I suggest we get rid of this. The guard layes will be
+        # overwritten anyway by `copy_guards()`.
+        self[:self.grid.lby, :] = 0.0
+        self[self.grid.uby:, :] = 0.0
+        self[:, self.grid.ubx:] = 0.0
+        self[:, :self.grid.lbx] = 0.0
